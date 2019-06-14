192334a5dcf699616532e8ea2cb85da9ddaa82a4
Toodyao-XPS-13 <toodyao@gmail.com>
2019-06-14 17:20:37 +0800

    Add user auth.

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -5,24 +5,23 @@ from flask_migrate import Migrate
 from config import Config
 
 
-# Flask-SQLAlchemy plugin
+# Flask-SQLAlchemy 插件
 db = SQLAlchemy()
-# # Flask-Migrate plugin
+# Flask-Migrate 插件
 migrate = Migrate()
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Enable CORS
+    # 启动CORS
     CORS(app)
-    # Init Flask-SQLAlchemy
+    # 初始化Flask-SQLAlchemy
     db.init_app(app)
-    # Init Flask-Migrate
+    # 初始化Flask-Migrate
     migrate.init_app(app, db)
 
-    # 注册 blueprint
+    # 注册blueprint
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
